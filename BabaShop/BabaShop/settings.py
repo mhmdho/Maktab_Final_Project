631fcdb4b688aa32af4361613b8ccf637015198d
mhmdho <hadiomidm@gmail.com>
2021-12-27 17:22:41 +0330

    set up installed apps in settings.py

--- BabaShop/BabaShop/settings.py
+++ BabaShop/BabaShop/settings.py
@@ -41,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'blog',
+    'user',
+    'order',
+    'shop',
 ]
 
 MIDDLEWARE = [
