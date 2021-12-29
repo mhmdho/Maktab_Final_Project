3feeff5787f35a1e22858378aa0cbcf11df1ed45
mhmdho <hadiomidm@gmail.com>
2021-12-29 17:30:48 +0330

    Set admin.py of myuser/order/shop apps

--- BabaShop/order/admin.py
+++ BabaShop/order/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Order, OrderItem, ProductComment, ProductLike
+
 # Register your models here.
+
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ProductComment)
+admin.site.register(ProductLike)
--- BabaShop/shop/admin.py
+++ BabaShop/shop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Product, ProductCategory, ProductTag, Shop
+
 # Register your models here.
+
+admin.site.register(Shop)
+admin.site.register(Product)
+admin.site.register(ProductCategory)
+admin.site.register(ProductTag)
