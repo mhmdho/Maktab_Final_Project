ff0612ab777936493dcd57872d2378c104775b2a
mhmdho <hadiomidm@gmail.com>
2021-12-29 18:08:12 +0330

    Add media and email config to settings.py

--- BabaShop/BabaShop/settings.py
+++ BabaShop/BabaShop/settings.py
@@ -138,3 +138,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'myuser.CustomUser'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'medai')
+MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
