fec3a426d32b3b79f906d6f13978ebaf9ebad0b6
mhmdho <hadiomidm@gmail.com>
2022-01-01 19:21:02 +0330

    Set down Shop list in view and template

--- BabaShop/shop/views.py
+++ BabaShop/shop/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from shop.models import Shop
 
 # Create your views here.
 
 
-class OrdersList(TemplateView):
+class OrdersList(ListView):
+    model = Shop
     template_name = 'shop/supplier_dashboard.html'
 
+    def get_queryset(self):
+        return Shop.Undeleted.filter(supplier=self.request.user
+                            ).order_by('id')
