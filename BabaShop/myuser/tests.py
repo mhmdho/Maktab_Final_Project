3b4a0dde5dd7d281a2761648e2ccfdb7411a8693
mhmdho <hadiomidm@gmail.com>
2022-01-14 00:54:30 +0330

    Test: customer_register create, password hash, password equality

--- BabaShop/myuser/tests.py
+++ BabaShop/myuser/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from django.contrib.auth.hashers import check_password
+
+from .models import CustomUser
+from model_mommy import mommy
 
 # Create your tests here.
+
+
+class TestPost(APITestCase):
+
+    def setUp(self):
+        pass
+
+    def test_customer_register(self):
+        url = reverse('customer_register_api')
+        data = {
+            "phone": "09901111111",
+            "email": "mohammad@gmail.com",
+            "username": "user111",
+            "password": "7ujmnhy6",
+            "password2": "7ujmnhy6"
+        }
+        resp = self.client.post(url, data=data)
+        self.assertEqual(resp.status_code, 201)
+
+    def test_customer_register_password_hash(self):
+        url = reverse('customer_register_api')
+        data = {
+            "phone": "09901111111",
+            "email": "mohammad@gmail.com",
+            "username": "user111",
+            "password": "7ujmnhy6",
+            "password2": "7ujmnhy6"
+        }
+        self.client.post(url, data=data)
+        customer = CustomUser.objects.get(phone="09901111111")
+        self.assertTrue(check_password("7ujmnhy6", customer.password))
+
+    def test_customer_register_wrong_password(self):
+        url = reverse('customer_register_api')
+        data = {
+            "phone": "09901111111",
+            "email": "mohammad@gmail.com",
+            "username": "user111",
+            "password": "7ujmnhy6",
+            "password2": "111"
+        }
+        resp = self.client.post(url, data=data)
+        self.assertEqual(resp.status_code, 400)
