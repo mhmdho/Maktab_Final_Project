1f8ce57bed3f741a485697ea1bbad9f2efa920bc
mhmdho <hadiomidm@gmail.com>
2022-01-14 01:17:55 +0330

    Test: customer_login password error

--- BabaShop/myuser/tests.py
+++ BabaShop/myuser/tests.py
@@ -1,15 +1,13 @@
-from django.contrib.auth import get_user_model
 from rest_framework.test import APITestCase
 from django.urls import reverse
-from django.contrib.auth.hashers import check_password
-
 from .models import CustomUser
 from model_mommy import mommy
+from django.contrib.auth.hashers import check_password
 
 # Create your tests here.
 
 
-class TestPost(APITestCase):
+class TestCustomerRegister(APITestCase):
 
     def setUp(self):
         pass
@@ -50,3 +48,31 @@ class TestPost(APITestCase):
         }
         resp = self.client.post(url, data=data)
         self.assertEqual(resp.status_code, 400)
+
+    def test_customer_register_is_customer(self):
+        url = reverse('customer_register_api')
+        data = {
+            "phone": "09901111111",
+            "email": "mohammad@gmail.com",
+            "username": "user111",
+            "password": "7ujmnhy6",
+            "password2": "7ujmnhy6"
+        }
+        self.client.post(url, data=data)
+        customer = CustomUser.objects.get(phone="09901111111")
+        self.assertTrue(customer.is_customer)
+
+
+class TestCustomerLogin(APITestCase):
+
+    def setUp(self):
+        self.user = mommy.make(CustomUser, phone='09901111112', password='7ujmnhy6')
+
+    def test_customer_login_error(self):
+        url = reverse('token_obtain_pair')
+        data = {
+            "phone": "09901111112",
+            "password": "222"
+        }
+        resp = self.client.post(url, data=data)
+        self.assertEqual(resp.status_code, 401)
